85193344959619ffc874cb9fe6fef40dda40ddff
Yama-Can <yama.can95@gmail.com>
2022-09-01 21:17:51 +0900

    [add]カタカナについての機能を追加,min
    ローマ字からカタカナ、カタカナからローマ字への変換を可能にした。
    
    (min)README.mdにサポート欄及びライセンス欄を追加した。

--- README.md
+++ README.md
@@ -11,8 +11,9 @@ Romaji/Japanese conversion tool
 ```cpp
 #include "rj-conversion.h"
 ```
+また、Visual Studioでは、<kbd>Shift</kbd><kbd>Alt</kbd><kbd>A</kbd>を押す必要がある場合があります。
 ## 関数
-### r_to_j()
+### r_to_h()
 これは、ローマ字を日本語のひらがなに変換します。
 例：
 ```cpp
@@ -21,14 +22,14 @@ Romaji/Japanese conversion tool
 using namespace std;
 using namespace rj_conversion;
 int main(){
-  cout << r_to_j("tukaikata");
+  cout << r_to_h("tukaikata");
 }
 ```
 結果：
 ```
 つかいかた
 ```
-### j_to_r()
+### h_to_r()
 これは、日本語のひらがなをローマ字に変換します。
 例：
 ```cpp
@@ -44,3 +45,45 @@ int main(){
 ```
 tukaikata
 ```
+### r_to_k()
+これは、ローマ字を日本語のカタカナに変換します。
+例：
+```cpp
+#include <iostream>
+#include "rj-conversion.h"
+using namespace std;
+using namespace rj_conversion;
+int main(){
+  cout << r_to_h("tukaikata");
+}
+```
+結果：
+```
+ツカイカタ
+```
+### k_to_r()
+これは、日本語のカタカナをローマ字に変換します。
+例：
+```cpp
+#include <iostream>
+#include "rj-conversion.h"
+using namespace std;
+using namespace rj_conversion;
+int main(){
+  cout << j_to_r("ツカイカタ");
+}
+```
+結果：
+```
+tukaikata
+```
+## Support
+yama.can@roisty.com
+## License
+Copyright 2022 Yama_can/Roisty
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- rj-conversion.h
+++ rj-conversion.h
@@ -3,23 +3,42 @@
 #include <vector>
 #include <utility>
 #include <regex>
+#define RJ_CONVERSION_VAL_SIZE 92
 namespace rj_conversion {
-    const std::vector<std::string> val1 = { "tya", "tyu", "tyo", "cha", "chu", "cho", "ja", "ju", "je", "jo", "fa", "fi", "fe", "fo", "qa", "qi", "qe", "qo", "ka", "ki", "ku", "ke", "ko", "ji","qu","fu","ca", "ci", "cu", "ce", "co", "sa", "si", "su", "se", "so", "ta", "ti", "tu", "te", "to", "na", "ni", "nu", "ne", "no", "ha", "hi", "hu", "he", "ho", "ma", "mi", "mu", "me", "mo", "ya", "yu", "yo", "ra", "ri", "ru", "re", "ro", "wa", "wi", "wu", "we", "wo", "nn", "ga", "gi", "gu", "ge", "go", "da", "di", "du", "de", "do","xa", "xi", "xu", "xe", "xo","xwa", "a", "i", "u", "e", "o", "n" };
-    const std::vector<std::string> val2 = { "ちゃ", "ちゅ", "ちょ", "ちゃ", "ちゅ", "ちょ", "じゃ", "じゅ", "じぇ", "じょ", "ふぁ", "ふぃ", "ふぇ", "ふぉ", "くぁ", "くぃ", "くぇ", "くぉ", "じ","く","ふ","か", "き", "く", "け", "こ", "か", "し", "く", "せ", "こ", "さ", "し", "す", "せ", "そ", "た", "ち", "つ", "て", "と", "な", "に", "ぬ", "ね", "の", "は", "ひ", "ふ", "へ", "ほ", "ま", "み", "む", "め", "も", "や", "ゆ", "よ", "ら", "り", "る", "れ", "ろ", "わ", "うぃ", "う", "うぇ", "を", "ん", "が", "ぎ", "ぐ", "げ", "ご", "だ", "ぢ", "づ", "で", "ど","ぁ", "ぃ", "ぅ", "ぇ", "ぉ","ゎ", "あ", "い", "う", "え", "お", "ん"};
-    std::string r_to_j(std::string romaji)
+    const std::vector<std::string> valr = { "tya", "tyu", "tyo", "cha", "chu", "cho", "ja", "ju", "je", "jo", "fa", "fi", "fe", "fo", "qa", "qi", "qe", "qo", "ka", "ki", "ku", "ke", "ko", "ji","qu","fu","ca", "ci", "cu", "ce", "co", "sa", "si", "su", "se", "so", "ta", "ti", "tu", "te", "to", "na", "ni", "nu", "ne", "no", "ha", "hi", "hu", "he", "ho", "ma", "mi", "mu", "me", "mo", "ya", "yu", "yo", "ra", "ri", "ru", "re", "ro", "wa", "wi", "wu", "we", "wo", "nn", "ga", "gi", "gu", "ge", "go", "da", "di", "du", "de", "do","xa", "xi", "xu", "xe", "xo","xwa", "a", "i", "u", "e", "o", "n" };
+    const std::vector<std::string> valh = { "ちゃ", "ちゅ", "ちょ", "ちゃ", "ちゅ", "ちょ", "じゃ", "じゅ", "じぇ", "じょ", "ふぁ", "ふぃ", "ふぇ", "ふぉ", "くぁ", "くぃ", "くぇ", "くぉ", "じ","く","ふ","か", "き", "く", "け", "こ", "か", "し", "く", "せ", "こ", "さ", "し", "す", "せ", "そ", "た", "ち", "つ", "て", "と", "な", "に", "ぬ", "ね", "の", "は", "ひ", "ふ", "へ", "ほ", "ま", "み", "む", "め", "も", "や", "ゆ", "よ", "ら", "り", "る", "れ", "ろ", "わ", "うぃ", "う", "うぇ", "を", "ん", "が", "ぎ", "ぐ", "げ", "ご", "だ", "ぢ", "づ", "で", "ど","ぁ", "ぃ", "ぅ", "ぇ", "ぉ","ゎ", "あ", "い", "う", "え", "お", "ん" };
+    const std::vector<std::string> valk = { "チャ", "チュ", "チョ", "チャ", "チュ", "チョ", "ジャ", "ジュ", "ジェ", "ジョ", "ファ", "フィ", "フェ", "フォ", "クァ", "クィ", "クェ", "クォ", "ジ","ク","ブ","カ", "キ", "ク", "ケ", "コ", "カ", "キ", "ク", "セ", "コ", "サ", "シ", "ス", "セ", "ソ", "タ", "チ", "ツ", "テ", "ト", "ナ", "ニ", "ヌ", "ネ", "ノ", "ハ", "ヒ", "フ", "ヘ", "ホ", "マ", "ミ", "ム", "メ", "モ", "ヤ", "ユ", "ヨ", "ラ", "リ", "ル", "レ", "ロ", "ワ", "ウィ", "ウ", "ウェ", "ヲ", "ン", "ガ", "ギ", "グ", "ゲ", "ゴ", "ダ", "ヂ", "ヅ", "デ", "ド","ァ", "ィ", "ゥ", "ェ", "ォ","ヮ", "ア", "イ", "ウ", "エ", "オ", "ン" };
+
+    std::string r_to_h(std::string romaji)
     {
-        for (int i = 0; i < val1.size(); i++)
+        for (int i = 0; i < RJ_CONVERSION_VAL_SIZE; i++)
         {
-            romaji = std::regex_replace(romaji, std::regex(val1.at(i)), val2.at(i));
+            romaji = std::regex_replace(romaji, std::regex(valr.at(i)), valh.at(i));
         }
         return romaji;
     }
-    std::string j_to_r(std::string japanese)
+    std::string r_to_k(std::string romaji)
     {
-        for (int i = 0; i < val1.size(); i++)
+        for (int i = 0; i < RJ_CONVERSION_VAL_SIZE; i++)
         {
-            japanese = std::regex_replace(japanese, std::regex(val2.at(i)), val1.at(i));
+            romaji = std::regex_replace(romaji, std::regex(valr.at(i)), valk.at(i));
         }
-        return japanese;
+        return romaji;
+    }
+    std::string h_to_r(std::string hiragana)
+    {
+        for (int i = 0; i < RJ_CONVERSION_VAL_SIZE; i++)
+        {
+            hiragana = std::regex_replace(hiragana, std::regex(valh.at(i)), valr.at(i));
+        }
+        return hiragana;
+    }
+    std::string k_to_r(std::string katakana)
+    {
+        for (int i = 0; i < RJ_CONVERSION_VAL_SIZE; i++)
+        {
+            katakana = std::regex_replace(katakana, std::regex(valk.at(i)), valr.at(i));
+        }
+        return katakana;
     }
 }
